6f38d309cf18eb1d515a89f27f16b360a9bdc6bc
Meshack <mbuvi.meshack99@gmail.com>
2022-08-22 07:28:12 +0300

    Completed assignment

--- test/indexTest.js
+++ test/indexTest.js
@@ -37,3 +37,10 @@ describe('index.js', function () {
     });
   });
 });
+let ride = 400
+if (ride <= 400){
+return "This one is on me!"}
+ else if (ride > 2000) {
+ return "I will gladly take your thirty bucks"}
+else if(ride > 2500){
+ return "No can do"}
